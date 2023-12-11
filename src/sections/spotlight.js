3c0ba6f7532cb1f3e79f95929503d8f23712cd0d
Vassili Philippov <vassiliphilippov@yandex.ru>
2023-12-11 08:54:08 +0000

    top alignment

--- src/sections/spotlight.js
+++ src/sections/spotlight.js
@@ -8,7 +8,7 @@ const Spotlight = () => {
             className="sm:py-10 md:py-20 lg:py-24 sm:px-4 lg:px-2 mx-auto text-left md:max-w-screen-md lg:max-w-screen-xl"
         >
             <div
-                className="grid sm:grid-cols-1 md:grid-rows-1 lg:grid-cols-[1.5fr,1fr] place-items-center md:gap-14"
+                className="grid sm:grid-cols-1 md:grid-rows-1 lg:grid-cols-[1.5fr,1fr]  md:gap-14"
             >
                 <div className="sm:order-2 lg:order-1">
                     <Fade bottom>
