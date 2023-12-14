099f2f7f3c7fb6c2c72fa5bf7caff81c130913fa
Vassili Philippov <vassiliphilippov@yandex.ru>
2023-12-14 19:07:52 +0000

    Switched link to Typeform

--- src/content/content.js
+++ src/content/content.js
@@ -27,7 +27,7 @@ const heroContent = {
   buttonText: 'Learn More',
   applyLink: '#helpyou',
   buttonTextTwo: 'Apply Now',
-  learnLink: 'https://forms.gle/Ch1uvcLqQ4FHAhS38',
+  learnLink: 'https://oxfordkaggle.typeform.com/to/v2SOj70O',
 };
 
 const brandContent = {
@@ -445,7 +445,7 @@ const libraryContent = {
 const applyContent = {
   title: "Ready to join our next cohort starting February 2024? Here's how to apply",
   buttonText: 'Apply Now',
-  buttonLink: 'https://forms.gle/Ch1uvcLqQ4FHAhS38',
+  buttonLink: 'https://oxfordkaggle.typeform.com/to/v2SOj70O',
   applyMethod: [
     {
       title: 'Application Method',
