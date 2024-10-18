7f1258ee586063607469dbc789943f474508baa9
gaurav <gaurav>
2024-10-18 18:43:00 +0530

    rebase website with old structure

--- next.config.js
+++ next.config.js
@@ -1,11 +1,4 @@
 /** @type {import('next').NextConfig} */
-// const nextConfig = {}
+const nextConfig = {}
 
-// module.exports = nextConfig
-// next.config.js
-module.exports = {
-    output: 'export', // Use 'export' for static export
-    basePath: '/oxfordkaggleclubwebsite', // Replace with your repo name
-    assetPrefix: '/oxfordkaggleclubwebsite/', // Replace with your repo name
-  };
-  
+module.exports = nextConfig
