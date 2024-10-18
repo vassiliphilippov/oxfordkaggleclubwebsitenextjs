c97f61e65f2343d82cb1faa4399c10fcae2e5360
gaurav <gaurav>
2024-10-18 11:09:51 +0530

    Deploy static Next.js site to GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+// const nextConfig = {}
 
-module.exports = nextConfig
+// module.exports = nextConfig
+// next.config.js
+module.exports = {
+    output: 'export', // Use 'export' for static export
+    basePath: '/oxfordkaggleclubwebsite', // Replace with your repo name
+    assetPrefix: '/oxfordkaggleclubwebsite/', // Replace with your repo name
+  };
+  
